5c254415a4d03db138800b6c4bdaba53ff981d05
shaiksha810 <Shaikshaikshavali7781@gmail.com>
2025-10-08 08:15:20 +0530

    solved getsavedreels

--- frontend/src/pages/Saved.jsx
+++ frontend/src/pages/Saved.jsx
@@ -2,17 +2,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL; // 👈 ye env se aayega
 
-
 const Saved = () => {
   const [savedReels, setSavedReels] = useState([]);
 
   useEffect(() => {
     axios
-      .get(`${API_URL}/api/food/saved` ,{ withCredentials: true })
+      .get(`${API_URL}/api/food/saved`, { withCredentials: true })
       .then((res) => {
-        setSavedReels(res.data.savedReels);
+        console.log("Response:", res.data);
+        const data = Array.isArray(res.data)
+          ? res.data
+          : res.data.savedReels || res.data.foodItems || [];
+        setSavedReels(data);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.error(err);
+        setSavedReels([]); // fallback
+      });
   }, []);
 
   return (
@@ -21,13 +27,7 @@ const Saved = () => {
       {savedReels.map((item) => (
         <div key={item._id} className="video-wrapper">
           {item.video ? (
-            <video
-              src={item.video}
-              className="video"
-              controls
-              loop
-              muted
-            />
+            <video src={item.video} className="video" controls loop muted />
           ) : (
             <img src={item.image} alt="food" className="video" />
           )}
